06ed6031b7db085794d719104cbfbb1f963e3f73
Nomanajmal007 <noman.ajmal@fliclicks.com>
2021-10-06 16:20:40 +0500

    Implemented edit and update funtionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,6 +1,6 @@
 class PortfoliosController < ApplicationController
     def index
-        @portfolio=Portfolio.all
+        @portfolio = Portfolio.all
     end
 
 
@@ -21,6 +21,23 @@ class PortfoliosController < ApplicationController
           end
         end
     end
+
+    def edit
+        @portfolio=Portfolio.find(params[:id])
+    end
+
+    def update
+        @portfolio=Portfolio.find(params[:id])
+        respond_to do |format|
+          if @portfolio.update(portfolio_params)
+            format.html { redirect_to portfolios_path, notice: "Portfolio was successfully updated." }
+            format.json { render :show, status: :ok, location: @portfolio }
+          else
+            format.html { render :edit, status: :unprocessable_entity }
+            format.json { render json: @portfolio.errors, status: :unprocessable_entity }
+          end
+        end
+      end
 
     private
 
