7c61ba0fe4943c9a6da91491e3da92e7d066a310
Nomanajmal007 <noman.ajmal@fliclicks.com>
2021-10-06 15:49:00 +0500

    Integrated new and create actions for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,30 @@ class PortfoliosController < ApplicationController
     def index
         @portfolio=Portfolio.all
     end
+
+
+    def new
+        @portfolio=Portfolio.new
+    end
+
+
+    def create
+        @portfolio = Portfolio.new(portfolio_params)
+        respond_to do |format|
+          if @portfolio.save
+            format.html { redirect_to portfolios_path, notice: "Your Portfolio is Live Now." }
+            format.json { render :show, status: :created, location: @portfolio }
+          else
+            format.html { render :new, status: :unprocessable_entity }
+            format.json { render json: @portfolio.errors, status: :unprocessable_entity }
+          end
+        end
+    end
+
+    private
+
+        def portfolio_params
+            params.require(:portfolio).permit(:title, :subtitle, :body)
+        end
+
 end
